cd18e0fad2e52e978c5805ed37a7a11dcc86f3fd
deepa sathish <deepa.sathish@gmail.com>
2020-03-19 14:39:08 +1100

    helloworld java file added

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+package myFirstJavaProgram;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		//void does not return anything
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld...");
+		//println gives extra line
+		System.out.print("This does not function print any line break....");
+		System.out.print("There is no line break");
+
+	}
+
+}
